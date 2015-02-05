dc8912d23f14c81eb1ba1d1425911d636a5bd152
Tiago Baptista <tbaptista@fnb.co.za>
2015-02-05 16:40:19 +0200

    zippy.py edit

--- python/exercises/zippy.py
+++ python/exercises/zippy.py
@@ -6,16 +6,19 @@
 one = ['swallow', 'snake', 'parrot']
 two = 'abc'
 three = '123'
+my_dict = {'name': 'Tiago', 'surname': 'Baptista', 'work': 'FNB'}
+
 
 def combo(*tup):
-	print tup[0]
-	print tup[1]
-
 	final_list = []
+	iterate = len(tup)
+
 	for index, field in enumerate(tup[0]):
-		#print '{} {}'.format(*field)
-		print field
-		print tup[1][index]
+		#print '{}: {}'.format(*field)
+
+		to_append = (field, tup[1][index])
+		final_list.append(to_append)
+		#print final_list
 		
 
 		#for value in field[1]:
@@ -23,4 +26,18 @@ def combo(*tup):
 
 
 
-combo(one, two)
+combo(one, two, three)
+
+def dict_test(one):
+	for step in enumerate(one):
+		print '{}: {}'.format(*step)
+
+
+dict_test(one)
+
+
+#Items() converts dict entries into keys:
+for key, value in my_dict.items():
+	#print '{}: {}'.format(key.title(), value)
+	#print key
+
