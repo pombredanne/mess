e2c6d67d58883735edaf88a860356e58cae8b674
SurrealTiggi <surrealtiggi@gmail.com>
2015-02-11 22:00:43 +0200

    Expanding dungeon

---
+++ python/exercises/character.py
@@ -0,0 +1,50 @@
+import random
+
+
+class Combat:
+	dodge_limit = 6
+	attack_limit = 6
+
+	def dodge(self):
+		roll = random.randint(1, self.dodge_limit)
+		return roll > 4
+
+	def attack(self):
+		roll = random.randint(1, self.attack_limit)
+		return roll > 4
+
+
+
+class Character(Combat):
+	attack_limit = 10
+	experience = 0
+	hit_points = 10
+
+	def attack(self):
+		roll = random.randint(1, self.attack_limit)
+		if self.weapon == 'sword':
+			roll += 1
+		elif self.weapon == 'axe':
+			roll += 2
+		return roll > 4
+
+
+	def get_weapon(self):
+		weapon_choice = raw_input("Weapon ([S]word, [A]xe, [B]ow): ").lower()
+
+		if weapon_choice in 'sab':
+			if weapon_choice == 's':
+				return 'sword'
+			elif weapon_choice == 'a':
+				return 'axe'
+			else:
+				return 'bow'
+		else:
+			return self.get_weapon()
+
+	def __init__(self, **kwargs):
+		self.name = raw_input("Name: ")
+		self.weapon = self.get_weapon()
+
+		for key,value in kwargs.items():
+			setattr(self, key, value)
--- python/exercises/monster.py
+++ python/exercises/monster.py
@@ -1,8 +1,9 @@
 import random
+from character import Combat
 
 COLORS = ['yellow', 'red', 'blue', 'green']
 
-class Monster:
+class Monster(Combat):
 	min_hit_points = 1
 	max_hit_points = 10
 	min_experience = 1
@@ -12,10 +13,16 @@ class Monster:
 
 	def __init__(self, **kwargs):
 		self.hit_points = random.randint(self.min_hit_points, self.max_hit_points)
+		self.experience = random.randint(self.min_experience, self.max_experience)
 		self.color = random.choice(COLORS)
 
 		for key, value in kwargs.items():
 			setattr(self, key, value)
+	def __str__(self):
+		return '{} {}, HP: {}, XP: {}'.format(self.color.title(),
+											  self.__class__.__name__,
+											  self.hit_points,
+											  self.experience)
 
 	def battlecry(self):
 		return self.sound.upper()
