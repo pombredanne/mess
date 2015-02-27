fe818002211abf8c222b7ff1f5288de843245829
Tiago Baptista <tbaptista@fnb.co.za>
2015-02-27 14:27:57 +0200

    Edit

--- python/exercises/quiz.py
+++ python/exercises/quiz.py
@@ -11,6 +11,13 @@ class Add(Question):
 
     def __init__(self, num1, num2):
         self.text = '{} + {}'.format(num1, num2)
+        self.answer = num1 + num2
+
+
+class Multiply(Question):
+
+    def __init__(self, num1, num2):
+        self.test = '{} * {}'.format(num1, num2)
         self.answer = num1 * num2
 
 
@@ -19,13 +26,31 @@ class Quiz:
     answers = []
 
     def __init__(self):
-    	# generate random questions
+        question_types = (Add, Multiply)
+        '''
+        The above can now be called as:
+        question_types[0](1, 5)
+        '''
+        for _ in range(10):
+            num1 = random.randint(1, 10)
+            num2 = random.randint(1, 10)
+            question = random.choice(question_types)(num1, num2)
+            self.questions.append(question)
 
     def take_quiz(self):
-    	#log start, ask questions
+        pass
+        # log start, ask questions
 
     def ask(self, question):
-    	pass
+        pass
+
+    def total_correct(self):
+        total = 0
+        for answer in self.answers:
+            if answer[0]:
+                total += 1
+        return total
 
     def summary(self):
-    	
+        print "You got {} out of {} right".format(self.total_correct(), len(self.questions))
+        print "It took you {} seconds total".format((self.end_time - self.start_time).seconds)
